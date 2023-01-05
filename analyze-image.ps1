14a27f1cef6644eaf6d67caf10fe12b5e5c136be
AIMAN64 <121802211+AIMAN64@users.noreply.github.com>
2023-01-05 16:51:59 +1100

    Create analyze-image.ps1

---
+++ analyze-image.ps1
@@ -0,0 +1,45 @@
+$key="YOUR_KEY"
+$endpoint="YOUR_ENDPOINT"
+
+
+# Code to call Computer Vision service for image analysis
+$img_file = "tan.jpg"
+if ($args.count -gt 0 -And $args[0] -in ("tan.jpg))
+{
+    $img_file = $args[0]
+}
+
+$img = "https://raw.githubusercontent.com/AIMAN64/Tan/$img_file"
+
+$headers = @{}
+$headers.Add( "Ocp-Apim-Subscription-Key", $key )
+$headers.Add( "Content-Type","application/json" )
+
+$body = "{'url' : '$img'}"
+
+write-host "Analyzing image..."
+$result = Invoke-RestMethod -Method Post `
+          -Uri "$endpoint/vision/v3.2/analyze?visualFeatures=Categories,Description,Objects" `
+          -Headers $headers `
+          -Body $body | ConvertTo-Json -Depth 5
+
+$analysis = $result | ConvertFrom-Json
+Write-Host("`nDescription:")
+foreach ($caption in $analysis.description.captions)
+{
+    Write-Host ($caption.text)
+}
+
+Write-Host("`nObjects in this image:")
+foreach ($object in $analysis.objects)
+{
+    Write-Host (" -", $object.object)
+}
+
+Write-Host("`nTags relevant to this image:")
+foreach ($tag in $analysis.description.tags)
+{
+    Write-Host (" -", $tag)
+}
+
+Write-Host("`n")
